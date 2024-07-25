19f098a6e6e69924ab175b634199ee94b04efbbb
cielcs <shotalicewww2@gmail.com>
2024-07-26 02:28:09 +0900

    create gif

---
+++ test/gif.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import os
+
+# PNGファイルがあるフォルダのパス
+folder_path = '../js/pic/'
+# 出力GIFのファイルパス
+output_path = 'simulation_v3.gif'
+
+# フォルダ内のPNGファイルを取得してソート
+files = sorted([f for f in os.listdir(folder_path) if f.endswith('.png')])
+
+# 画像を開いてリストに追加
+images = [Image.open(os.path.join(folder_path, file)) for file in files]
+
+# GIFを作成
+images[0].save(output_path, save_all=True, append_images=images[1:], optimize=False, duration=500, loop=0)
