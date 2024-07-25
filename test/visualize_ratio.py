b3d2da98e4c3ae56ef5e700cda9b4debed906144
齋藤正博 <saitoumasahiro@ws23is059.naist.jp>
2024-07-25 18:33:26 +0900

    calc_ratio

---
+++ test/visualize_ratio.py
@@ -0,0 +1,16 @@
+import os
+import pandas as pd
+import matplotlib as plt
+from path_info import DATA_DIR
+
+
+
+df = pd.read_csv(os.path.join(DATA_DIR, 'prefecture_data_with_pairs_info.csv'))
+
+df['male_ratio'] = df['Male_Subs'] / df['Male_Population']
+df['femal_ratio'] = df['Female_subs'] / df['Female_Population']
+df['total_ratio'] = df['Total_subs'] / df['Total_Population']
+
+#print(df)
+
+df.to_csv(os.path.join(DATA_DIR, 'prefecture_data_with_pairs_info_v2.csv'))
