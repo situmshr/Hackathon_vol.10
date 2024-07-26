243bbc72b1c777a2e98e34d7f3ba1bc3fa7ab479
cielcs <shotalicewww2@gmail.com>
2024-07-26 15:15:22 +0900

    add v5

--- test/gif.py
+++ test/gif.py
@@ -1,14 +1,21 @@
 from PIL import Image
 import os
-from path_info import 
+import re
 
 # PNGファイルがあるフォルダのパス
 folder_path = '../js/picv5/'
 # 出力GIFのファイルパス
-output_path = 'simulation_v3.gif'
+output_path = 'gif/simulation_v5.gif'
+
+# 数字を抽出するための正規表現パターン
+pattern = re.compile(r'\d+')
+
+def sort_key(file_name):
+    match = pattern.search(file_name)
+    return int(match.group()) if match else -1
 
 # フォルダ内のPNGファイルを取得してソート
-files = sorted([f for f in os.listdir(folder_path) if f.endswith('.png')])
+files = sorted([f for f in os.listdir(folder_path) if f.endswith('.png')], key=sort_key)
 
 # 画像を開いてリストに追加
 images = [Image.open(os.path.join(folder_path, file)) for file in files]
