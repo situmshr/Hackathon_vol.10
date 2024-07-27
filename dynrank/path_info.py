579e534751b1fc0f8916d6fafe184020944165b2
agoyr <yamauchi.ryoga116@gmail.com>
2024-07-28 08:26:46 +0900

    modify path_info for module

--- dynrank/path_info.py
+++ dynrank/path_info.py
@@ -2,10 +2,14 @@ import os
 
 # temp = __file__.rfind('/', 0, __file__.rfind('/'))
 # prefix = __file__[:temp]
-prefix = os.getcwd()
-PROJECT_DIR = prefix
-DATA_DIR = os.path.join(prefix, "data")
-OUTPUT_DIR = os.path.join(prefix, "results")
+# prefix = os.getcwd()
+# PROJECT_DIR = prefix
+# DATA_DIR = os.path.join(prefix, "data")
+# OUTPUT_DIR = os.path.join(prefix, "results")
+def path_parser(data_dir,output_dir,project_dir):
+    data_abs_dir = os.path.join(project_dir,'data',data_dir)
+    output_abs_dir = os.path.join(project_dir,'results',output_dir)
+    return data_abs_dir, output_abs_dir
 
 
 # if __name__ == "__main__":
